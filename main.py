b06447761ffd06b6d8555b4f4bfb2e9647f54604
username <54479676+CormacKrum@users.noreply.github.com>
2020-10-20 04:01:02 +0530

    Initial Commit

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
-board = [' ' for _ in range(10)]
+board = [' ' for _ in range(9)]
 
 def insertAtPosition(letter, pos):
     board[pos] = letter
 
-def printBoard(board):
+def printBoard():
     print("     |     |     ")
-    print("  "+board[1]+"  |  "+board[2]+"  |  "+board[3]+"  ")
-    print("     |     |     ")
-    print("-----------------")
-    print("     |     |     ")
-    print("  "+board[4]+"  |  "+board[5]+"  |  "+board[6]+"  ")
+    print("  "+board[0]+"  |  "+board[1]+"  |  "+board[2]+"  ")
     print("     |     |     ")
     print("-----------------")
     print("     |     |     ")
-    print("  "+board[7]+"  |  "+board[8]+"  |  "+board[9]+"  ")
+    print("  "+board[3]+"  |  "+board[4]+"  |  "+board[5]+"  ")
+    print("     |     |     ")
+    print("-----------------")
+    print("     |     |     ")
+    print("  "+board[6]+"  |  "+board[7]+"  |  "+board[8]+"  ")
     print("     |     |     ")
     print("\n")
 
+def safeIsFree(pos):
+    return board[pos]==' '
 
-printBoard(board)
+def isBoardFull():
+    return board.count(' ')==0
+
